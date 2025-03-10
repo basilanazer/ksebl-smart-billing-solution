7ad1fe8ab9934172ca881b8e8ed9d3406aacfc6e
AnaghaJn21 <anaghajayan21@gmail.com>
2025-03-10 23:31:29 +0530

    Modiifications

--- smart_billing/lib/screens/dashboard.dart
+++ smart_billing/lib/screens/dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:smart_billing/model/logout.dart';
 
 class Dashboard extends StatefulWidget {
@@ -8,25 +10,21 @@ class Dashboard extends StatefulWidget {
   State<Dashboard> createState() => _DashboardState();
 }
 
+Future<void> addItems(Map<String, dynamic> dets, String id) async {
+  return await FirebaseFirestore.instance
+      .collection("1114567890123")
+      .doc(id)
+      .set(dets);
+}
+
 class _DashboardState extends State<Dashboard> {
-  // Future addItems(Map<String, dynamic> dets, String id) async {
-  //   return await FirebaseFirestore.instance
-  //       .collection("1112345671234")
-  //       .doc(id)
-  //       .set(dets);
-  // }
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    //USE THIS PART TO ADD DUMMY VALUES
-    // addItems({
-    //   "Bill Date": "06.10.2020",
-    //   "Current Reading": "22255",
-    //   "Previous Reading": "22176",
-    //   "Units Consumed": "79",
-    //   "Amount Payable": "454",
-    // }, "2020-10");
-
     return SafeArea(
         child: WillPopScope(
       onWillPop: () async {
@@ -36,7 +34,9 @@ class _DashboardState extends State<Dashboard> {
           builder: (context) => AlertDialog(
             //backgroundColor: Colors.amber[50],
             title: const Text("Exit"),
-            content: const Text('Are you sure you want to exit?',),
+            content: const Text(
+              'Are you sure you want to exit?',
+            ),
             actions: <Widget>[
               TextButton(
                 onPressed: () {
@@ -45,7 +45,8 @@ class _DashboardState extends State<Dashboard> {
                 },
                 child: const Text(
                   'Yes',
-                  style: TextStyle(color: Color(0xFF4D4C7D), fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      color: Color(0xFF4D4C7D), fontWeight: FontWeight.bold),
                 ),
               ),
               TextButton(
@@ -55,10 +56,10 @@ class _DashboardState extends State<Dashboard> {
                 },
                 child: const Text(
                   'No',
-                  style: TextStyle(color: Color(0xFFFD7250), fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      color: Color(0xFFFD7250), fontWeight: FontWeight.bold),
                 ),
               ),
-              
             ],
           ),
         );
@@ -67,188 +68,189 @@ class _DashboardState extends State<Dashboard> {
         return exit;
       },
       child: Scaffold(
-      appBar: AppBar(
-        actions: [
-          //logout
-          IconButton(
-            onPressed: () {
-              confirmLogout(context);
-            },
-            icon: const Icon(
-              Icons.logout,
-              color: Color(0xFFFD7250),
+        appBar: AppBar(
+          actions: [
+            //logout
+            IconButton(
+              onPressed: () {
+                confirmLogout(context);
+              },
+              icon: const Icon(
+                Icons.logout,
+                color: Color(0xFFFD7250),
+              ),
             ),
+            //analytics
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/analytics');
+              },
+              icon: const Icon(
+                Icons.line_axis,
+                color: Color(0xFFFD7250),
+              ),
+            ),
+            //notifications
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.notifications_active_outlined,
+                color: Color(0xFFFD7250),
+              ),
+            ),
+            //profile
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/profile');
+              },
+              icon: const Icon(
+                Icons.person_outline,
+                color: Color(0xFFFD7250),
+              ),
+            ),
+          ],
+          title: const Text(
+            'DashBoard',
           ),
-          //analytics
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/analytics');
-            },
-            icon: const Icon(
-              Icons.line_axis,
-              color: Color(0xFFFD7250),
-            ),
-          ),
-          //notifications
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.notifications_active_outlined,
-              color: Color(0xFFFD7250),
-            ),
-          ),
-          //profile
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/profile');
-            },
-            icon: const Icon(
-              Icons.person_outline,
-              color: Color(0xFFFD7250),
-            ),
-          ),
-        ],
-        title: const Text('DashBoard',),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-            Center(
-              child: Image.asset(
-                'assets/icon.png',
-                width: 264,
-                height: 112,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(30),
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color(0xFF4D4C7D),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const Icon(
-                    Icons.qr_code_scanner,
-                    color: Color(0xFFFD7250),
-                    size: 40,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      //Navigator.of(context).pushReplacementNamed('/capture');
-                      Navigator.of(context).pushNamed('/barcode');
-                    }, // Opens the camera
-                    child: const Text(
-                      "Capture Your Meter Now",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const Icon(
-                    Icons.arrow_forward,
-                    color: Color(0xFFFD7250),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(30),
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color(0xFFFD7250),
-              ),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(
-                    Icons.file_copy,
-                    color: Colors.white,
-                    size: 40,
-                  ),
-                  Text(
-                    "Next Bill Due 10th April",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0xFF4D4C7D),
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 30),
-                child: Text(
-                  "Last Month Analytics",
-                  style: TextStyle(
-                      color: Color(0xFF4D4C7D),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              Center(
+                child: Image.asset(
+                  'assets/icon.png',
+                  width: 264,
+                  height: 112,
                 ),
               ),
-            ),
-            Container(
-              //height: 100,
-              margin: const EdgeInsets.all(30),
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color.fromARGB(255, 189, 188, 231),
-              ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Unit: ",
+              Container(
+                margin: const EdgeInsets.all(30),
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF4D4C7D),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Icon(
+                      Icons.qr_code_scanner,
+                      color: Color(0xFFFD7250),
+                      size: 40,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        //Navigator.of(context).pushReplacementNamed('/capture');
+                        Navigator.of(context).pushNamed('/barcode');
+                      }, // Opens the camera
+                      child: const Text(
+                        "Capture Your Meter Now",
                         style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      Text(
-                        "235 KW",
-                        style: TextStyle(
-                            color: Color(0xFF4D4C7D),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Amount(Rs): ",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "1640.00",
-                        style: TextStyle(
-                            color: Color(0xFF4D4C7D),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20),
-                      )
-                    ],
-                  ),
-                ],
+                    ),
+                    const Icon(
+                      Icons.arrow_forward,
+                      color: Color(0xFFFD7250),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            // Your existing code continues...
-          ],
+              Container(
+                margin: const EdgeInsets.all(30),
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFFFD7250),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.file_copy,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                    Text(
+                      "Next Bill Due 10th April",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF4D4C7D),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Text(
+                    "Last Month Analytics",
+                    style: TextStyle(
+                        color: Color(0xFF4D4C7D),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                //height: 100,
+                margin: const EdgeInsets.all(30),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 189, 188, 231),
+                ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Unit: ",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "235 KW",
+                          style: TextStyle(
+                              color: Color(0xFF4D4C7D),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Amount(Rs): ",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "1640.00",
+                          style: TextStyle(
+                              color: Color(0xFF4D4C7D),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              // Your existing code continues...
+            ],
+          ),
         ),
       ),
-    ),
-    )
-  );
+    ));
   }
 }
